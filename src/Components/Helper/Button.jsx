09f5aea002a1a47b8ed112e6ff4ae908dec0ad58
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2023-09-21 21:47:18 -0800

    Delete src directory

--- src/Components/Helper/Button.jsx
+++
@@ -1,8 +0,0 @@
-
-const Button = (props) => {
-  return <>
-<button className="hero-btn">{props.name}</button>
-  </>;
-};
-
-export default Button;
--- src/Components/Helper/Wrapper.jsx
+++
@@ -1,5 +0,0 @@
-const Wrapper = (props) => {
-  return <div className='container ' style={{marginBottom : '5rem'}}>{props.children}</div>;
-};
-
-export default Wrapper;
